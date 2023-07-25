a99b93391883bcd185e0e12db7e7fa39e9c9d564
WenJJ2000 <wenjj2000@gmail.com>
2023-07-25 16:34:04 +0800

    july 2023 update

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,6 +1,7 @@
 import ABC_LOGO from "../img/project/AblockLogo.jpg";
 import poster from "../img/project/Poster_design.jpg";
 import des from "../img/project/des_pic.jpg";
+import web3 from "../img/project/web3_proj.jpg";
 
 const portfolios = [
   {
@@ -22,6 +23,15 @@ const portfolios = [
     text: "Shop simulator, completed as part of CS2030",
     links: {
       github: "https://github.com/WenJJ2000/CS2030",
+    },
+  },
+  {
+    title: "Web3 app (In Progress)",
+    image: web3,
+    categories: ["React", "JavaScript", "Software Engineering", "firebase"],
+    text: "Web App to display NFT Transactions",
+    links: {
+      github: "https://github.com/WenJJ2000/Web3.0",
     },
   },
 ];
